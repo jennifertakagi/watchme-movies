b2f514d478f0802ba2883c58152b2ec00a02ab55
jennifertakagi <jennitakagi@gmail.com>
2021-05-01 20:57:11 -0300

    feat: add Content component

---
+++ src/components/Content.tsx
@@ -0,0 +1,32 @@
+import {  MovieProps, GenreResponseProps } from '../types/constants';
+
+import { MovieCard } from './MovieCard';
+
+interface ContentProps {
+  movies: MovieProps[];
+  selectedGenre: GenreResponseProps;
+}
+
+export function Content({ movies, selectedGenre }: ContentProps) {
+  return (
+    <div className="container">
+      <header>
+        <span className="category">Category:<span> {selectedGenre.title}</span></span>
+      </header>
+
+      <main>
+        <div className="movies-list">
+          {movies.map(movie => (
+            <MovieCard
+              key ={movie.imdbID}
+              title={movie.Title}
+              poster={movie.Poster}
+              runtime={movie.Runtime}
+              rating={movie.Ratings[0].Value}
+            />
+          ))}
+        </div>
+      </main>
+    </div>
+  );
+}
